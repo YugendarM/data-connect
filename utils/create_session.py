37599f99aa2ccb17e5b5719883b2bc2ea0a50c1b
YugendarM <yugendar2411@gmail.com>
2025-04-09 13:36:15 +0530

    Update - update on secrets handling in session creation

--- utils/create_session.py
+++ utils/create_session.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from snowflake.snowpark import Session
+import os
+
+# @st.cache_resource
+# def create_session():
+#     return Session.builder.configs(st.secrets.snowflake).create()
+
 
 @st.cache_resource
 def create_session():
-    return Session.builder.configs(st.secrets.snowflake).create()
+    connection_parameters = {
+        "user": os.getenv("SNOWFLAKE_USER"),
+        "password": os.getenv("SNOWFLAKE_PASSWORD"),
+        "account": os.getenv("SNOWFLAKE_ACCOUNT")
+    }
+    return Session.builder.configs(connection_parameters).create()
